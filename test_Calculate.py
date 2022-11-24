e7274e869f68762d891513b7b86a52b424ab0d94
MORVAN Loïs <lois.morvan@laposte.net>
2022-11-24 11:20:36 +0100

    10

--- test_Calculate.py
+++ test_Calculate.py
@@ -3,7 +3,7 @@ import calculate
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculate.add(8, 2)
+        assert 10 == calculate.add(8, 2)
 
     def test_subtraction(self):
         assert 2 == calculate.subtract(4, 2)
